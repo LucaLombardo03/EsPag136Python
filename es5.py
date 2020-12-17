3350d458d5e586f39a5da48aefc9b2d8c8beeb03
luca <lucalombardo03@gmail.com>
2020-12-17 10:26:03 +0100

    fine app

--- es5.py
+++ es5.py
@@ -1,5 +1,9 @@
-f=1
-
-for i in range(5,0,-1):
-    f=f*i
-print(f)
+import sys
+if len(sys.argv)!=2:
+    print("python3 es5.py <parametro>")
+else:
+    number = int(sys.argv[1])
+    fatt = 1
+    for i in range(number, 0, -1):
+        fatt = fatt * i
+    print(f"Il fattoriale di {number} é {fatt}")
